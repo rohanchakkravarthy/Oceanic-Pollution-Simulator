0c619a86be6ae6ec9bc6ddb63919e48e5e0cfe75
rohanchakkravarthy <rohanchakkravarthy@gmail.com>
2026-03-14 18:09:56 -0400

    Updated code for the simulator in Javascript

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const pollutant ={
   radius: 16
 };
 
+// Draws Circles
 function drawCircle(obj) {
   ctx.beginPath();
   ctx.arc(obj.x, obj.y, obj.radius, 0, Math.PI * 2);
@@ -27,6 +28,7 @@ function drawCircle(obj) {
   ctx.fill();
 }
 
+// Loops the Drawings
 function draw() {
   ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, Width, Height);
@@ -37,18 +39,35 @@ function draw() {
   requestAnimationFrame(draw);
 }
 
+// Fish Horisontal and Vertical Speed
+fish.speedX = Math.random() * 2 + 1;
+fish.speedY = Math.random() * 1 - 0.5;
+
+// Moves Fish
+fish.x += fish.speedX;
+fish.y += fish.speedY;
+
+// Makes Screen Wrap Around the Sides
+if (fish.x > Width) fish.x = 0;
+if (fish.x < 0) fish.x = Width;
+
+// Keeps Fish Above the Sand Level
+if (fish.y > Height - 100) fish.y = Height - 100;
+if (fish.y < 0) fish.y = 0;
+
+// Makes a Variety of Unique Fish
+const fishList = [];
+for (let i = 0; i < 10; i++) {
+  fishList.push({
+    x: Math.random() * Width,
+    y: Math.random() * Height - 100,
+    radius: Math.random() * 20 + 10,
+    color: ["orange", "yellow","pink"][Math.floor(Math.random()*3)],
+    speedX: Math.random() * 2 + 1,
+    speedY: Math.random() * 1 - 0.5,
+  });
+}
+
+
+  
 draw();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
